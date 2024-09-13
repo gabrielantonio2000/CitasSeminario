1c5630a85cb936f9e05aab6d58b1350599f5fb3c
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-09-12 23:43:13 -0600

    cambios

--- Backend/config.js
+++ Backend/config.js
@@ -14,7 +14,3 @@ export const PASS = process.env.PASS;
 export const SERVER = process.env.SERVER;
 export const DB = process.env.DB;
 
-//CONEXION MONGO
-export const MONGO_URL = process.env.MONGO_URL;
-
-export const HOST_FRONT = process.env.HOST_FRONT ?? "http://localhost:5173";
