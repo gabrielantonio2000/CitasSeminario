a2ea52215275a8d08084375e04058496b3d64816
Edras <monterrosoedras@gmail.com>
2024-09-25 13:43:02 -0600

    se agregaron utilidades

---
+++ citasBack/utils/diasDeLaSemana.js
@@ -0,0 +1,20 @@
+export const diasDeLaSemana = [
+  "domingo",
+  "lunes",
+  "martes",
+  "miercoles",
+  "jueves",
+  "viernes",
+  "sabado",
+];
+
+// Mapeo de nombres de días de la semana
+export const diaMap = {
+  lunes: "lunes",
+  martes: "martes",
+  miercoles: "miercoles",
+  jueves: "jueves",
+  viernes: "viernes",
+  sabado: "sabado",
+  domingo: "domingo",
+};
