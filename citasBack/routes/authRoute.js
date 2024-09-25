193e6a345935c8c6cd7ea7c9d66a8cced820cdfb
Edras <monterrosoedras@gmail.com>
2024-09-25 13:42:04 -0600

    se agrego los controladores de los endpoints

---
+++ citasBack/routes/authRoute.js
@@ -0,0 +1,15 @@
+// import { Router } from "express";
+// import {
+//   registrarUsuario,
+//   loginUsuario,
+// } from "../controllers/authController.js";
+// import { estaLogueado } from "../middleware/authMiddleware.js";
+
+// const authRouter = Router();
+
+// authRouter.post("/registro", registrarUsuario);
+// authRouter.post("/login", loginUsuario);
+
+// // authRouter.post("/reportes", estaLogueado, loginUsuario);
+
+// export default authRouter;
