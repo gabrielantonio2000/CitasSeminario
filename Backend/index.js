13ba1c1cff726070f590fdaef276615b8308ba9e
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-09-25 19:37:22 -0600

    actualizar

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,10 @@
 import { PORT } from "./config.js";
+//los acabo de agregar
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+
 
 
 app.listen(PORT, console.log(`server ejecutandose en el puerto ${PORT}`));
